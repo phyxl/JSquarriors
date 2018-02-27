9fa7af37121db1dfa6a1193f020f61ab771cf492
phyxl01 <phyxl01@gmail.com>
2018-02-27 11:06:05 -0600

    getting started

---
+++ game.js
@@ -0,0 +1,13 @@
+//this is just a starting point file for the quarriors implementation
+//
+
+
+function main(){
+	//grab the elements,
+	//define the players,
+	//define the starting game,
+	//take turns,
+	//check for victory conditions
+	//
+	return 0; //I think this is optional in js
+}
